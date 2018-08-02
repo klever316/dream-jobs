a674a5554bbf54141f04a62f549d324638eddf3a
Ronaldo <kikogamer@gmail.com>
2018-08-01 22:33:47 -0300

    created vagas service

--- spec/services/programathor_service_spec.rb
+++ spec/services/programathor_service_spec.rb
@@ -3,7 +3,7 @@ require './app/services/programathor_service'
 
 describe ProgramathorService do
   it 'successfully scrap' do
-    VCR.use_cassette("web_scrap/jobs") do
+    VCR.use_cassette("web_scrap/jobs_programathor") do
       jobs = subject.jobs
       expect(jobs).not_to be_nil
       expect(jobs.count).to eql(10)
--- spec/services/vagas_service_spec.rb
+++ spec/services/vagas_service_spec.rb
@@ -3,7 +3,7 @@ require './app/services/vagas_service'
 
 describe VagasService do
   it 'successfully scrap' do
-    VCR.use_cassette("web_scrap/jobs") do
+    VCR.use_cassette("web_scrap/jobs_vagas") do
       jobs = subject.jobs
       expect(jobs).not_to be_nil
       expect(jobs.count).to eql(40)
